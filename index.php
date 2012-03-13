de98353730cb19e06dbb748ee4396cc178db4f5f
Proyecto <saenztandil@gmail.com>
2012-03-13 11:43:28 -0300

    primer intento

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <script type="text/javascript" src="jquery-1.7.1.js"></script>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
     </head>
