611332793cc7098268719a779cc41743b399146a
Proyecto <saenztandil@gmail.com>
2012-03-14 11:38:20 -0300

    Key Points:  Completo

--- index.php
+++ index.php
@@ -6,10 +6,17 @@
     </head>
     <body>
     <header>Globant Bootcamp HTML5</header>
-    <h1>Learning Days</h1>
     <article>
-        <a href="http://localhost/app/day1/index.html">Day 1<br></a>
-        Day 2<br>
+        <h1>Learning Days</h1>
+        <h2>Day 1<br></h2>
+            <a href="http://localhost/app/day1/2.html">Practice 2<br></a>
+            <a href="http://localhost/app/day1/3.html">Practice 3<br></a>
+            <a href="http://localhost/app/day1/4.html">Practice 4<br></a>
+            <a href="http://localhost/app/day1/5.html"><b>Practice 5</b><br></a>
+            <a href="http://localhost/app/day1/6.html"><b>Practice 6</b><br></a>
+            <a href="http://localhost/app/day1/7.html"><b>Practice 7</b><br></a>
+            
+         <h2>Day 2<br></h2>
     </article>
     </body>
 </html>
