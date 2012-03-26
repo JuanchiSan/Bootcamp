f96c86a146512794d55bc594519737200eb23696
Proyecto <saenztandil@gmail.com>
2012-03-26 14:59:31 -0300

    Tema 6 [inic]

--- index.php
+++ index.php
@@ -9,27 +9,28 @@
     <article>
         <h1>Learning Days</h1>
         <h2>Day 1: Javascript Intro and jQuery<br></h2>
-            <a href="http://localhost/Bootcamp/day1/2.html">Practice 2<br></a>
-            <a href="http://localhost/Bootcamp/day1/3.html">Practice 3<br></a>
-            <a href="http://localhost/Bootcamp/day1/4.html">Practice 4<br></a>
-            <a href="http://localhost/Bootcamp/day1/5.html"><b>Practice 5</b><br></a>
-            <a href="http://localhost/Bootcamp/day1/6.html"><b>Practice 6</b><br></a>
-            <a href="http://localhost/Bootcamp/day1/7.html"><b>Practice 7</b><br></a>
-            <a href="http://localhost/Bootcamp/day1/8.html"><b>Practice 8</b><br></a>
-            <a href="http://localhost/Bootcamp/day1/9.html"><b>Practice 9</b><br></a>
-            <a href="http://localhost/Bootcamp/day1/10.html"><b>Practice 10</b><br></a>
+            <a href="http://localhost/Bootcamp/day01/2.html">Practice 2<br></a>
+            <a href="http://localhost/Bootcamp/day01/3.html">Practice 3<br></a>
+            <a href="http://localhost/Bootcamp/day01/4.html">Practice 4<br></a>
+            <a href="http://localhost/Bootcamp/day01/5.html"><b>Practice 5</b><br></a>
+            <a href="http://localhost/Bootcamp/day01/6.html"><b>Practice 6</b><br></a>
+            <a href="http://localhost/Bootcamp/day01/7.html"><b>Practice 7</b><br></a>
+            <a href="http://localhost/Bootcamp/day01/8.html"><b>Practice 8</b><br></a>
+            <a href="http://localhost/Bootcamp/day01/9.html"><b>Practice 9</b><br></a>
+            <a href="http://localhost/Bootcamp/day01/10.html"><b>Practice 10</b><br></a>
          <h2>Day 2 & 3: jQuery Mobile<br></h2>
-            <a href="http://localhost/Bootcamp/day2-3/1-5.html">Practice 1-5<br></a>
-            <a href="http://localhost/Bootcamp/day2-3/6-8.html">Practice 6-8<br></a>
-            <a href="http://localhost/Bootcamp/day2-3/9-10.html">Practice 9-10<br></a>
-            <a href="http://localhost/Bootcamp/day2-3/11-12.html">Practice 11-12<br></a>
+            <a href="http://localhost/Bootcamp/day02-03/1-5.html">Practice 1-5<br></a>
+            <a href="http://localhost/Bootcamp/day02-03/6-8.html">Practice 6-8<br></a>
+            <a href="http://localhost/Bootcamp/day02-03/9-10.html">Practice 9-10<br></a>
+            <a href="http://localhost/Bootcamp/day02-03/11-12.html">Practice 11-12<br></a>
          <h2>Day 4, 5 & 6: Design Patterns and OOP in Javascript<br></h2>
-            <a href="http://localhost/Bootcamp/day4-6/index.html">Practice 1-10<br></a>
+            <a href="http://localhost/Bootcamp/day04-06/index.html">Practice 1-10<br></a>
          <h2>Day 7: AMD + RequireJS<br></h2>
-            <a href="http://localhost/Bootcamp/day7/index.php">Practice 1-8<br></a>
+            <a href="http://localhost/Bootcamp/day07/index.php">Practice 1-8<br></a>
          <h2>Day 8: Template Engines<br></h2>
-            <a href="http://localhost/Bootcamp/day8/index.html">Practice 1-7<br></a>
-            
+            <a href="http://localhost/Bootcamp/day08/index.html">Practice 1-7<br></a>
+         <h2>Day 9,10: MVC & Mixins<br></h2>
+            <a href="http://localhost/Bootcamp/day09-10/index.html">Practice 1-7<br></a>   
     </article>
     </body>
 </html>
