56d7714b656cb0dfdcc0532576c8b078d4ce35df
Proyecto <saenztandil@gmail.com>
2012-03-26 14:54:18 -0300

    Tema 5 Completo

--- Day8/Scripts/main.js
+++ Day8/Scripts/main.js
@@ -8,6 +8,7 @@ var tMustache = "<div>\
                     <p>Place of Birth: {{placeBirth}}</p>\
                     <p>Citizenship: {{citizen}}</p>\
                     <p>Sex: {{sex}}</p>\
+                    <p>Education: </p>\
                     {{#Education}}<ul data-role='listview' data-theme='d' id='EduList'>\
                         <li><p>Institution: {{einstitution}}</p>\
                         <p>Degree: {{edegree}}</p>\
@@ -24,33 +25,39 @@ var tUnderscore = "<div>\
                     <p>Place of Birth: <%=Sections.placeBirth%></p>\
                     <p>Citizenship: <%=Sections.citizen%></p>\
                     <p>Sex: <%=Sections.sex%></p>\
+                    <p>Education: </p>\
                     <ul data-role='listview' data-theme='d' id='EduList'>\
-                    <% _.each(Sections.Education, function(einstitution,edegree,eperiod){%>\
-                    <li><p>Institution: <% print(Sections.Education.einstitution);%></p>\
-                        <p>Degree: <%=Sections.Education.edegree%></p>\
-                        <p>Period: <%=Sections.Education.eperiod%></p>\
-                    </li><%});%></ul></div>";
+                    <%for(var j=0; j<Sections.Education.length;j++){ %>\
+                    <%var edu = Sections.Education[j];%> \
+                    <li><p>Institution: <%=edu.einstitution%></p>\
+                        <p>Degree: <%=edu.edegree%></p>\
+                        <p>Period: <%=edu.eperiod%></p>\
+                    </li><%};%></ul></div>";
 
 var tjsmart = "<div>\
-                    {{#Sections}}<p>Name: {{name}}</p>\
-                    <p>Address: {{address}}</p>\
-                    <p>Telephone: {{telephone}}</p>\
-                    <p>Cell Phone: {{cellphone}}</p>\
-                    <p>Email: {{email}}</p>\
-                    <p>Date of Birth: {{dateBirth}}</p>\
-                    <p>Place of Birth: {{placeBirth}}</p>\
-                    <p>Citizenship: {{citizen}}</p>\
-                    <p>Sex: {{sex}}</p>\
-                    {{#Education}}<ul data-role='listview' data-theme='d' id='EduList'>\
-                        <li><p>Institution: {{einstitution}}</p>\
-                        <p>Degree: {{edegree}}</p>\
-                        <p>Period: {{eperiod}}</p>\
-                        </li></ul>{{/Education}}{{/Sections}}</div>";
+                <p>Name: {$Sections.name}</p>\
+                <p>Address: {$Sections.address}</p>\
+                <p>Telephone: {$Sections.telephone}</p>\
+                <p>Cell Phone: {$Sections.cellphone}</p>\
+                <p>Email: {$Sections.email}</p>\
+                <p>Date of Birth: {$Sections.dateBirth}</p>\
+                <p>Place of Birth: {$Sections.placeBirth}</p>\
+                <p>Citizenship: {$Sections.citizen}</p>\
+                <p>Sex: {$Sections.sex}</p>\
+                <p>Education: </p>\
+                <ul data-role='listview' data-theme='d' id='EduList'>\
+                    {foreach $Sections.Education as $j => $edu}\
+                    <li><p>Institution: {$edu.einstitution}</p>\
+                    <p>Degree: {$edu.edegree}</p>\
+                    <p>Period: {$edu.eperiod}</p>\
+                </li>{/foreach}</ul></div>";
 
 $(document).ready(function() {
 var html1 = Mustache.render(tMustache, data);
 $('#template1').html(html1);
 var html2 = _.template(tUnderscore, data);
 $('#template2').html(html2);
-
+var html3 = new jSmart(tjsmart);
+var code = html3.fetch( data );
+$('#template3').html(code);
  });
