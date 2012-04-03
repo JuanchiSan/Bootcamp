520ed21346f759a7fd0dfbdc4d68f32c705d9430
Proyecto <saenztandil@gmail.com>
2012-04-03 18:42:37 -0300

    correccion topic 4

--- Day07/scripts/main.js
+++ Day07/scripts/main.js
@@ -1,5 +1,10 @@
+require.config({
+        paths : {
+            jquery : 'js/jquery-jqmobile'
+        }
+});
 
-require(["modules/Director", "modules/movie"], function(Director, Movie) {
+require(["modules/Director", "modules/movie", "jquery"], function(Director, Movie) {
                 var movie = new Movie ("Alien",8, 123);                
                 var RidleyScott = new Director("Ridley Scott",["Cast is everything.", "Do what you haven't..."]);
                 movie.setDirector(RidleyScott);
