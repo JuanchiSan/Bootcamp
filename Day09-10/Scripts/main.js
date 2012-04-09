c48c8078811efbb9942cada50c156fd8c5758f40
Proyecto <saenztandil@gmail.com>
2012-04-09 01:11:29 -0300

    correccion topic 6

--- Day09-10/Scripts/main.js
+++ Day09-10/Scripts/main.js
@@ -1,93 +1,95 @@
-// Load the application once the DOM is ready, using `jQuery.ready`:
 $(function(){
-
-// Movie Model
-// ----------
+            // Movie Model
+            // ----------
+            
     var Movie = Backbone.Model.extend({
         defaults:{
             title: "default",
-            ReleaseYear: 0,
-            Synopsis: "default"
+            ReleaseYear: '0',
+            Synopsis: "Synopsis for default"
         },
-        
         initialize: function(){
-            console.log('this model has been initialized');
-        },
+            if (!this.get("title")) {
+                    this.set({"title": this.defaults.title});
+                }
+                if (!this.get("ReleaseYear")) {
+                    this.set({"ReleaseYear": this.defaults.ReleaseYear});
+                }
+                if (!this.get("Synopsis")) {
+                    this.set({"Synopsis": this.defaults.Synopsis});
+                }
+            console.log('This model has been initialized with the name '+ this.get('title'));
         
+        },
         clear: function() {
-            this.destroy();     // #destroy is provided by Backbone.js
-        //  this.view.remove(); // will explain in a few slides
+            this.destroy();     
         } 
     });
     
-// FavoriteMovies Collection
-// -------------------------
+            // FavoriteMovies Collection
+            // -------------------------
+            
     var FavoriteMovies = Backbone.Collection.extend({
         model: Movie
     });
     
+    
  // Create our global collection of **favoritemovies**.
  
     var favoritemovies = new FavoriteMovies();
-
-// Movie Item View
-// --------------
+    
+            // Movie Item View
+            // --------------
 
     var MovieView = Backbone.View.extend({
 
-        //... is a list tag.
         tagName:  "li",
-
-        // The DOM events specific to an item.
+        template: _.template($('#item-template').html()),
         events: {
         "click .delbtt"     : "remove",
-        "click .detbtt"     : "details",
         "click .editbtt"    : "edit",
-        'keypress #editMovie': 'editOnEnter'
+        "keypress .edit"  : "editOnEnter",
+        "keypress .editY"      : "editOnEnter",
+        "keypress .editS"      : "editOnEnter"
         },
-
-        initialize: function() {      
+        initialize: function() {
             this.model.bind('change', this.render, this);
-            _.bindAll(this,'remove','details', 'edit');
             this.model.bind('remove',this.unrender,this);           
         },
-        
         render: function(){          
-            var newTitle = "<li>"+this.model.get("title");
-            var Editbtt = "<span><input type='button' value='Edit' class='editbtt'></input><span>"
-            var Delbtt = "<span><input type='button' value='Delete' class='delbtt'></input></span>";
-            var Detbtt = "<span><input type='button' value='Details' class='detbtt'></input></span></li>";
-            $(this.el).html(newTitle + Editbtt + Delbtt + Detbtt);
+            this.$el.html(this.template(this.model.toJSON()));
+            this.input = this.$('.edit');
             return this;
 	},
-        
         unrender: function(){
             $(this.el).remove();
         },
-        
-        remove: function(){
+	remove: function(){
             this.model.destroy();
         },
-        
-        details: function(){
-            var _title=this.model.get("title");
-            var year=this.model.get("ReleaseYear");
-            var syno=this.model.get("Synopsis");
-            var details="<p><b>"+_title+"</b>("+year+")</p><p>"+syno+"</p>";
-            $("#detailsMovies").html(details);
-        },
-        
         edit: function(){
-            
-            this.$el.html("<input type='text' id='editMovie'></input>");
-            $("#editMovie").focus();
+            this.$el.addClass("editing");
         },
-        
+        close: function() {
+            this.input = this.$('.edit');
+            var value = this.input.val();
+            if (!value) this.clear();
+            this.model.set({title: value});
+
+            this.input = this.$('.editY');
+            value = this.input.val();
+            if (!value) this.clear();
+            this.model.set({ReleaseYear: value});
+
+            this.input = this.$('.editS');
+            value = this.input.val();
+            if (!value) this.clear();
+            this.model.set({Synopsis: value});
+            
+            this.$el.removeClass("editing");
+        },
         editOnEnter: function(e){
-            if (e.keyCode != 13) return; 
-            if (!$("#editMovie").val()) return;
-            var newValue=$("#editMovie").val();
-            this.model.set({ title: newValue });
+            if (e.keyCode == 13) this.close();
         }
     });   
     
@@ -95,32 +97,45 @@ $(function(){
 // --------------
 
     AppView = Backbone.View.extend({
-
+        
+        el : $("#create-list"),
 	events: {
         "keypress #new-movie":  "createOnEnter"
   	},
-
 	initialize: function(){
 		favoritemovies.bind('add',this.addOne);
 		this.collection=favoritemovies;
 	},
-        
-	createOnEnter: function(e){
-            if (e.keyCode != 13) return; 
-            if (!$("#new-movie").val()) return;
-            var mov= new Movie;
-            mov.set({title: $("#new-movie").val()});
-            this.collection.add(mov); 
-            $("#new-movie").val('');
+	render: function() {
+            
+      if (Movies.length) {
+        this.main.show();
+        this.footer.show();
+      } else {
+        this.main.hide();
+        this.footer.hide();
+      }
         },
-        
+        createOnEnter: function(e) {
+            if (e.keyCode != 13) return;
+            var mov = this.$el.find('#new-movie').val();
+            if (!mov) return;
+            favoritemovies.add({title: mov});
+            this.$el.find('#new-movie').val('');
+        },
+
         addOne: function(mov){
             var view= new MovieView({model: mov});
-            $("#list").append(view.render().el);
+            $("#movie-list").append(view.render().el);
         }
-    }); 
-    
-    var app = new AppView({el : $("#create-list")});  
+    });
+    var app = new AppView(); 
+    favoritemovies.add([{title: 'Back to the future', ReleaseYear: '1985', Synopsis: 'In 1985, Doc Brown invents time travel; in 1955, Marty McFly accidentally prevents his parents from meeting, putting his own existence at stake.'},
+                        {title: 'Jurassic Park', ReleaseYear: '1993', Synopsis: 'During a preview tour, a theme park suffers a major power breakdown that allows its cloned dinosaur exhibits to run amok.'},
+                        {title: 'Terminator 2: Judgment Day', ReleaseYear: '1991', Synopsis: 'The cyborg who once tried to kill Sarah Connor must now protect her teenage son, John Connor, from an even more powerful and advanced Terminator.'},
+                        {title: 'Raiders of the Lost Ark', ReleaseYear: '1981', Synopsis: 'Archeologist and adventurer Indiana Jones is hired by the US government to find the Ark of the Covenant before the Nazis.'}    
+                   ]);
+    console.log('loadding');
 });
 
 
